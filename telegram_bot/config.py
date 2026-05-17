b115655b899b1978b1675df0177572e7432f34a7
suprimkina <anna.suprun.0022@mail.ru>
2026-05-18 01:46:16 +0300

    Delete bot configuration from config.py
    Removed bot configuration settings and token.

--- telegram_bot/config.py
+++ telegram_bot/config.py
@@ -1,16 +1,1 @@
-import os
-from dotenv import load_dotenv
 
-load_dotenv()
-
-# Токен бота (получите у @BotFather)
-BOT_TOKEN = os.getenv('BOT_TOKEN', '8572835537:AAHXnSkQ_ElpO0t3_p99ugg13gszEgrm7o8')
-
-# Путь к базе данных
-DATABASE_PATH = 'it_jobs.db'
-
-# Настройки бота
-BOT_SETTINGS = {
-    'parse_mode': 'HTML',
-    'disable_web_page_preview': True
-}
